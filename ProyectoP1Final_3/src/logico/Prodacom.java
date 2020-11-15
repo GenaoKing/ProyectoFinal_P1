5d805dbf8ca85fa7fed4f44c7415941f0f4584a4
GenaoKing <20181847@ce.pucmm.edu.do>
2020-11-15 09:08:50 -0400

    metodos de buscar e insertar objetos.

--- ProyectoP1Final_3/src/logico/Prodacom.java
+++ ProyectoP1Final_3/src/logico/Prodacom.java
@@ -1,6 +1,7 @@
 package logico;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Prodacom {
 	
@@ -30,41 +31,82 @@ public class Prodacom {
 		return combos;
 	}
 
-	public void setCombos(ArrayList<Combo> combos) {
-		this.combos = combos;
+	public void insertarCombo(Combo combo) {
+		combos.add(combo);
 	}
 
 	public ArrayList<Componente> getComponentes() {
 		return componentes;
 	}
 
-	public void setComponentes(ArrayList<Componente> componentes) {
-		this.componentes = componentes;
+	public void insertarComponente(Componente componente) {
+		this.componentes.add(componente); 
 	}
 
 	public ArrayList<Factura> getFacturas() {
 		return facturas;
 	}
 
-	public void setFacturas(ArrayList<Factura> facturas) {
-		this.facturas = facturas;
+	public void insertarFactura(Factura factura) {
+		this.facturas.add(factura); 
 	}
 
 	public ArrayList<Cliente> getClientes() {
 		return clientes;
 	}
 
-	public void setClientes(ArrayList<Cliente> clientes) {
-		this.clientes = clientes;
+	public void insertarCliente(Cliente cliente) {
+		this.clientes.add(cliente);
 	}
 
 	public ArrayList<Usuario> getUsuarios() {
 		return usuarios;
 	}
 
-	public void setUsuarios(ArrayList<Usuario> usuarios) {
-		this.usuarios = usuarios;
+	public void insertarUsuario(Usuario usuario) {
+		this.usuarios.add(usuario);
 	}
 	
+	public Cliente buscarCliente(String cedula) {
+		Cliente cliente = null;
+		int i = 0;
+		boolean encontrado = false;
+		while (i<clientes.size() && !encontrado) {
+			if(clientes.get(i).getCedula().equalsIgnoreCase(cedula)) {
+				encontrado = true;
+				cliente = clientes.get(i);
+			}
+			i++;
+		}
+		return cliente;
+	}
+	
+	public Factura buscarFactura(String codigo) {
+		Factura factura = null;
+		int i = 0;
+		boolean encontrado = false;
+		while (i<facturas.size() && !encontrado) {
+			if(facturas.get(i).getCod().equalsIgnoreCase(codigo)) {
+				encontrado = true;
+				factura = facturas.get(i);
+			}
+			i++;
+		}
+		return factura;
+	}
+	
+	public Componente buscarComponente(String serie) {
+		Componente componente = null;
+		int i = 0;
+		boolean encontrado = false;
+		while (i<componentes.size() && !encontrado) {
+			if(componentes.get(i).getSerie().equalsIgnoreCase(serie)) {
+				encontrado = true;
+				componente = componentes.get(i);
+			}
+			i++;
+		}
+		return componente;
+	}
 	
 }
