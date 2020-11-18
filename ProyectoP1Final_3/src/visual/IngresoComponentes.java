e8c99bdffdba80f5b4d7ce8cbbcb2363199e4a79
xavier-source <20181590@ce.pucmm.edu.do>
2020-11-17 23:32:32 -0400

    Arreglo al ingr.De component

--- ProyectoP1Final_3/src/visual/IngresoComponentes.java
+++ ProyectoP1Final_3/src/visual/IngresoComponentes.java
@@ -17,13 +17,25 @@ import java.awt.Color;
 import javax.swing.JCheckBox;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JScrollPane;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
 
 public class IngresoComponentes extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
+	private JTextField txtSerie;
+	private JTextField txtModelo;
+	private JTextField txtMarca;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JSpinner spnCantMaxima;
+	private JSpinner spnCantReal;
+	private JSpinner spnCantMinima;
+	private JSpinner spnCompra;
+	private JLabel lblPrecioVenta;
+	private JLabel lblConector;
 
 	/**
 	 * Launch the application.
@@ -55,86 +67,130 @@ public class IngresoComponentes extends JDialog {
 		panel.setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("Serie:");
-		lblNewLabel.setBounds(34, 25, 46, 14);
+		lblNewLabel.setBounds(10, 25, 46, 14);
 		panel.add(lblNewLabel);
 		
 		JLabel lblModelo = new JLabel("Modelo:");
-		lblModelo.setBounds(273, 25, 46, 14);
+		lblModelo.setBounds(146, 25, 46, 14);
 		panel.add(lblModelo);
 		
 		JLabel lblMarca = new JLabel("Marca:");
-		lblMarca.setBounds(512, 25, 46, 14);
+		lblMarca.setBounds(306, 25, 46, 14);
 		panel.add(lblMarca);
 		
-		textField = new JTextField();
-		textField.setBounds(75, 22, 127, 20);
-		panel.add(textField);
-		textField.setColumns(10);
+		txtSerie = new JTextField();
+		txtSerie.setBounds(50, 22, 86, 20);
+		panel.add(txtSerie);
+		txtSerie.setColumns(10);
 		
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
-		textField_1.setBounds(329, 22, 127, 20);
-		panel.add(textField_1);
+		txtModelo = new JTextField();
+		txtModelo.setColumns(10);
+		txtModelo.setBounds(202, 22, 86, 20);
+		panel.add(txtModelo);
 		
-		textField_2 = new JTextField();
-		textField_2.setColumns(10);
-		textField_2.setBounds(568, 22, 127, 20);
-		panel.add(textField_2);
+		txtMarca = new JTextField();
+		txtMarca.setColumns(10);
+		txtMarca.setBounds(354, 22, 86, 20);
+		panel.add(txtMarca);
 		
-		JLabel lblPrecio = new JLabel("Precio:");
-		lblPrecio.setBounds(75, 78, 64, 14);
+		JLabel lblPrecio = new JLabel("Compra:");
+		lblPrecio.setBounds(450, 25, 58, 14);
 		panel.add(lblPrecio);
 		
-		JLabel lblCantidad = new JLabel("Cantidad:");
-		lblCantidad.setBounds(414, 78, 64, 14);
+		JLabel lblCantidad = new JLabel("Cantidad M\u00EDnima:");
+		lblCantidad.setBounds(10, 75, 99, 14);
 		panel.add(lblCantidad);
 		
-		JSpinner spinner = new JSpinner();
-		spinner.setBounds(122, 75, 127, 20);
-		panel.add(spinner);
+		spnCompra = new JSpinner();
+		spnCompra.setModel(new SpinnerNumberModel(new Float(1), new Float(1), null, new Float(1)));
+		spnCompra.setBounds(500, 22, 89, 20);
+		panel.add(spnCompra);
 		
-		JSpinner spinner_1 = new JSpinner();
-		spinner_1.setBounds(474, 75, 127, 20);
-		panel.add(spinner_1);
+		spnCantMinima = new JSpinner();
+		spnCantMinima.setModel(new SpinnerNumberModel(new Integer(1), new Integer(1), null, new Integer(1)));
+		spnCantMinima.setBounds(101, 72, 127, 20);
+		panel.add(spnCantMinima);
+		
+		lblPrecioVenta = new JLabel("Venta:");
+		lblPrecioVenta.setBounds(600, 25, 76, 14);
+		panel.add(lblPrecioVenta);
+		
+		JSpinner spnVenta = new JSpinner();
+		spnVenta.setModel(new SpinnerNumberModel(new Float(1), new Float(1), null, new Float(1)));
+		spnVenta.setBounds(642, 22, 89, 20);
+		panel.add(spnVenta);
+		
+		JLabel lblCantidadReal = new JLabel("Cantidad Real:");
+		lblCantidadReal.setBounds(253, 75, 99, 14);
+		panel.add(lblCantidadReal);
+		
+		spnCantReal = new JSpinner();
+		spnCantReal.setModel(new SpinnerNumberModel(new Integer(1), new Integer(1), null, new Integer(1)));
+		spnCantReal.setBounds(352, 72, 127, 20);
+		panel.add(spnCantReal);
+		
+		JLabel lblCantidadMxima = new JLabel("Cantidad M\u00E1xima:");
+		lblCantidadMxima.setBounds(513, 75, 99, 14);
+		panel.add(lblCantidadMxima);
+		
+		spnCantMaxima = new JSpinner();
+		spnCantMaxima.setModel(new SpinnerNumberModel(new Integer(1), new Integer(1), null, new Integer(1)));
+		spnCantMaxima.setBounds(604, 72, 127, 20);
+		panel.add(spnCantMaxima);
 		
 		JPanel panel_1 = new JPanel();
 		panel_1.setBorder(new TitledBorder(null, "Seleccione", TitledBorder.CENTER, TitledBorder.TOP, null, null));
-		panel_1.setBounds(10, 139, 753, 117);
+		panel_1.setBounds(10, 139, 753, 57);
 		contentPanel.add(panel_1);
 		panel_1.setLayout(null);
 		
 		JRadioButton rdbtnNewRadioButton = new JRadioButton("MemoriaRam");
-		rdbtnNewRadioButton.setBounds(168, 23, 109, 23);
+		rdbtnNewRadioButton.setBounds(245, 23, 109, 23);
 		panel_1.add(rdbtnNewRadioButton);
 		
 		JRadioButton rdbtnDisco = new JRadioButton("Disco");
-		rdbtnDisco.setBounds(168, 69, 109, 23);
+		rdbtnDisco.setBounds(589, 23, 109, 23);
 		panel_1.add(rdbtnDisco);
 		
 		JRadioButton rdbtnMicroprocesadores = new JRadioButton("Microprocesadores");
-		rdbtnMicroprocesadores.setBounds(445, 69, 129, 23);
+		rdbtnMicroprocesadores.setBounds(407, 23, 129, 23);
 		panel_1.add(rdbtnMicroprocesadores);
 		
 		JRadioButton rdbtnMotherboard = new JRadioButton("MotherBoard");
-		rdbtnMotherboard.setBounds(445, 23, 139, 23);
+		rdbtnMotherboard.setSelected(true);
+		rdbtnMotherboard.setBounds(53, 23, 139, 23);
 		panel_1.add(rdbtnMotherboard);
 		
 		JPanel panel_2 = new JPanel();
-		panel_2.setBounds(10, 267, 753, 117);
+		panel_2.setBounds(10, 207, 753, 177);
 		contentPanel.add(panel_2);
 		panel_2.setLayout(null);
 		
-		JCheckBox chckbxNewCheckBox = new JCheckBox("New check box");
-		chckbxNewCheckBox.setBounds(67, 47, 97, 23);
-		panel_2.add(chckbxNewCheckBox);
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(570, 11, 173, 155);
+		panel_2.add(scrollPane);
 		
-		JCheckBox checkBox = new JCheckBox("New check box");
-		checkBox.setBounds(214, 47, 97, 23);
-		panel_2.add(checkBox);
+		JScrollPane scrollPane_1 = new JScrollPane();
+		scrollPane_1.setBounds(334, 11, 173, 155);
+		panel_2.add(scrollPane_1);
 		
-		JCheckBox checkBox_1 = new JCheckBox("New check box");
-		checkBox_1.setBounds(387, 47, 97, 23);
-		panel_2.add(checkBox_1);
+		lblConector = new JLabel("Conexion");
+		lblConector.setBounds(10, 49, 64, 14);
+		panel_2.add(lblConector);
+		
+		JLabel lblTipoRam = new JLabel("TipoRam:");
+		lblTipoRam.setBounds(10, 112, 64, 14);
+		panel_2.add(lblTipoRam);
+		
+		textField_3 = new JTextField();
+		textField_3.setBounds(66, 46, 125, 20);
+		panel_2.add(textField_3);
+		textField_3.setColumns(10);
+		
+		textField_4 = new JTextField();
+		textField_4.setColumns(10);
+		textField_4.setBounds(66, 109, 125, 20);
+		panel_2.add(textField_4);
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
