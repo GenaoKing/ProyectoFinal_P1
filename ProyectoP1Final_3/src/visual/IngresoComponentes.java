7e1497c98b385d2f517f34b93ebfc3dc01cd9200
GenaoKing <20181847@ce.pucmm.edu.do>
2020-11-18 00:38:27 -0400

    Registrar Conexiones motherboard

--- ProyectoP1Final_3/src/visual/IngresoComponentes.java
+++ ProyectoP1Final_3/src/visual/IngresoComponentes.java
@@ -12,15 +12,26 @@ import javax.swing.JTextField;
 import javax.swing.JSpinner;
 import javax.swing.JRadioButton;
 import javax.swing.border.TitledBorder;
+import javax.swing.table.DefaultTableModel;
+
+
+
 import javax.swing.UIManager;
 import java.awt.Color;
 import javax.swing.JCheckBox;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.awt.event.ActionEvent;
 import javax.swing.JScrollPane;
 import javax.swing.SpinnerNumberModel;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class IngresoComponentes extends JDialog {
 
@@ -28,14 +39,27 @@ public class IngresoComponentes extends JDialog {
 	private JTextField txtSerie;
 	private JTextField txtModelo;
 	private JTextField txtMarca;
-	private JTextField textField_3;
-	private JTextField textField_4;
+	private JTextField txtConexion;
+	private JTextField txtTipoRam;
 	private JSpinner spnCantMaxima;
 	private JSpinner spnCantReal;
 	private JSpinner spnCantMinima;
 	private JSpinner spnCompra;
 	private JLabel lblPrecioVenta;
 	private JLabel lblConector;
+	private JTable table;
+	private JTable table_1;
+	private JButton btnAgregar;
+	private JButton btnQuitar;
+	private JLabel lblTipoRam;
+	public static DefaultTableModel modelo;
+	public static DefaultTableModel modelo_1;
+	public static Object[] fila;
+	private ArrayList<String> conexiones;
+	public String q=null;
+	private ArrayList<String>agregados = new ArrayList<String>();
+	private int cont = 0;
+	private JButton okButton;
 
 	/**
 	 * Launch the application.
@@ -54,7 +78,7 @@ public class IngresoComponentes extends JDialog {
 	 * Create the dialog.
 	 */
 	public IngresoComponentes() {
-		setBounds(100, 100, 789, 477);
+		setBounds(100, 100, 789, 563);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
@@ -153,7 +177,7 @@ public class IngresoComponentes extends JDialog {
 		panel_1.add(rdbtnDisco);
 		
 		JRadioButton rdbtnMicroprocesadores = new JRadioButton("Microprocesadores");
-		rdbtnMicroprocesadores.setBounds(407, 23, 129, 23);
+		rdbtnMicroprocesadores.setBounds(407, 23, 147, 23);
 		panel_1.add(rdbtnMicroprocesadores);
 		
 		JRadioButton rdbtnMotherboard = new JRadioButton("MotherBoard");
@@ -162,41 +186,151 @@ public class IngresoComponentes extends JDialog {
 		panel_1.add(rdbtnMotherboard);
 		
 		JPanel panel_2 = new JPanel();
-		panel_2.setBounds(10, 207, 753, 177);
+		panel_2.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Informacion Adicional", TitledBorder.CENTER, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		panel_2.setBounds(10, 207, 753, 261);
 		contentPanel.add(panel_2);
 		panel_2.setLayout(null);
 		
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(570, 11, 173, 155);
-		panel_2.add(scrollPane);
+		modelo_1 = new DefaultTableModel();
+		String []columns2 = {"Nombre"};
+		modelo_1.setColumnIdentifiers(columns2);
 		
-		JScrollPane scrollPane_1 = new JScrollPane();
-		scrollPane_1.setBounds(334, 11, 173, 155);
-		panel_2.add(scrollPane_1);
+		modelo = new DefaultTableModel();
+		String array[]={"USB-C","USB-A","Jack 3.5 mm","SATA","PCIe","I/O","HDMI","Ethernet","VGA","Micro-SD","SD"};
+		conexiones =new ArrayList<String>( Arrays.asList(array));
+		String []columns = {"Nombre"};
+		modelo.setColumnIdentifiers(columns);
 		
-		lblConector = new JLabel("Conexion");
+		lblConector = new JLabel("Conexion:");
 		lblConector.setBounds(10, 49, 64, 14);
 		panel_2.add(lblConector);
 		
-		JLabel lblTipoRam = new JLabel("TipoRam:");
+		lblTipoRam = new JLabel("TipoRam:");
 		lblTipoRam.setBounds(10, 112, 64, 14);
 		panel_2.add(lblTipoRam);
 		
-		textField_3 = new JTextField();
-		textField_3.setBounds(66, 46, 125, 20);
-		panel_2.add(textField_3);
-		textField_3.setColumns(10);
+		txtConexion = new JTextField();
+		txtConexion.setBounds(87, 46, 125, 20);
+		panel_2.add(txtConexion);
+		txtConexion.setColumns(10);
 		
-		textField_4 = new JTextField();
-		textField_4.setColumns(10);
-		textField_4.setBounds(66, 109, 125, 20);
-		panel_2.add(textField_4);
+		txtTipoRam = new JTextField();
+		txtTipoRam.setColumns(10);
+		txtTipoRam.setBounds(87, 109, 125, 20);
+		panel_2.add(txtTipoRam);
+		
+		btnAgregar = new JButton(">>");
+		btnAgregar.setEnabled(false);
+		btnAgregar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				cont++;
+				conexiones.remove(q);
+				agregados.add(q);
+				CargarTabla();
+				CargarTablaAgregados();
+				btnAgregar.setEnabled(false);
+				if(cont>0) {
+					okButton.setEnabled(true);
+				}else {
+					okButton.setEnabled(false);
+				}
+				
+			}
+		});
+		btnAgregar.setBounds(445, 66, 80, 25);
+		panel_2.add(btnAgregar);
+		
+		btnQuitar = new JButton("<<");
+		btnQuitar.setEnabled(false);
+		btnQuitar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				cont--;
+				conexiones.add(q);
+				agregados.remove(q);
+				CargarTabla();
+				CargarTablaAgregados();
+				btnQuitar.setEnabled(false);
+				if(cont>0) {
+					okButton.setEnabled(true);
+				}else {
+					okButton.setEnabled(false);
+				}
+				
+			}
+		});
+		btnQuitar.setBounds(445, 175, 80, 25);
+		panel_2.add(btnQuitar);
+		
+		JPanel panel_3 = new JPanel();
+		panel_3.setBounds(227, 37, 192, 211);
+		panel_2.add(panel_3);
+		panel_3.setLayout(new BorderLayout(0, 0));
+		
+		JScrollPane scrollPane_1 = new JScrollPane();
+		panel_3.add(scrollPane_1, BorderLayout.CENTER);
+		scrollPane_1.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		table = new JTable();
+		table.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				
+				int seleccion = table.getSelectedRow();
+				int modelrow = table.convertRowIndexToModel(seleccion);
+				if(seleccion!=-1){
+					btnAgregar.setEnabled(true);
+					btnQuitar.setEnabled(false);
+					q = conexiones.get(seleccion);
+				}else{
+					btnAgregar.setEnabled(false);
+					btnQuitar.setEnabled(false);
+					q = null;
+				}
+			}
+		});
+		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		table.setModel(modelo);
+		scrollPane_1.setViewportView(table);
+		
+		JPanel panel_4 = new JPanel();
+		panel_4.setBounds(549, 37, 192, 211);
+		panel_2.add(panel_4);
+		panel_4.setLayout(new BorderLayout(0, 0));
+		
+		JScrollPane scrollPane = new JScrollPane();
+		panel_4.add(scrollPane, BorderLayout.CENTER);
+		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		table_1 = new JTable();
+		table_1.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				int seleccion = table_1.getSelectedRow();
+				int modelrow = table_1.convertRowIndexToModel(seleccion);
+				if(seleccion!=-1){
+					btnAgregar.setEnabled(false);
+					btnQuitar.setEnabled(true);
+					
+					q = agregados.get(seleccion);
+				}else{
+					btnAgregar.setEnabled(false);
+					btnQuitar.setEnabled(false);
+					q = null;
+				}
+			}
+		});
+		table_1.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		table_1.setModel(modelo_1);
+		scrollPane.setViewportView(table_1);
+		
+		JPanel panel_disponibles = new JPanel();
+		panel_disponibles.setBounds(0, 0, 10, 10);
+		contentPanel.add(panel_disponibles);
+		panel_disponibles.setLayout(new BorderLayout(0, 0));
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
 			getContentPane().add(buttonPane, BorderLayout.SOUTH);
 			{
-				JButton okButton = new JButton("Registrar");
+				okButton = new JButton("Registrar");
 				okButton.setActionCommand("OK");
 				buttonPane.add(okButton);
 				getRootPane().setDefaultButton(okButton);
@@ -205,12 +339,36 @@ public class IngresoComponentes extends JDialog {
 				JButton cancelButton = new JButton("Cancel");
 				cancelButton.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
+						for(String s:agregados) {
+							conexiones.add(s);
+						}
 						dispose();
 					}
 				});
 				cancelButton.setActionCommand("Cancel");
 				buttonPane.add(cancelButton);
 			}
+		}
+		CargarTabla();
+		CargarTablaAgregados();
+	}
+
+	private void CargarTablaAgregados() {
+		modelo_1.setRowCount(0);
+		fila = new Object[modelo_1.getColumnCount()];
+		for(String e:agregados) {
+			fila[0]=e;
+			modelo_1.addRow(fila);
+		}
+		
+	}
+
+	private void CargarTabla() {
+		modelo.setRowCount(0);
+		fila = new Object[modelo.getColumnCount()];
+		for(String e: conexiones) {
+			fila[0]=e;
+			modelo.addRow(fila);
 		}
 	}
 }
